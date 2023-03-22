d0ab5164a516359919f64c058582328cb3f299ef
Leeloop <leon@leeloop.si>
2023-03-22 13:38:08 +0000

    clock

--- resources/views/dashboard/create-event.blade.php
+++ resources/views/dashboard/create-event.blade.php
@@ -19,13 +19,13 @@
                     <label class="text-white font-bold" for="event_start">Začetek dela</label>
                     <div class="mt-2 py-2 bg-transparent rounded-lg text-gray-100">
                         <div class="flex">
-                        <select name="event_hours_start" class="bg-transparent text-xl appearance-none border rounded-lg border-gray-400">
+                        <select id="event_hours_start" name="event_hours_start" class="bg-transparent text-xl appearance-none border rounded-lg border-gray-400">
                             @foreach (range(00, 23) as $item) {
                             <option class="bg-gray-700" value="{{ $item }}">{{ $item }}</option>
                             @endforeach
                         </select>
                         <span class="text-xl p-2">:</span>
-                        <select name="event_minutes_start" class="bg-transparent text-xl appearance-none border rounded-lg border-gray-400">
+                        <select id="event_minutes_start" name="event_minutes_start" class="bg-transparent text-xl appearance-none border rounded-lg border-gray-400">
                             <option class="bg-gray-700" value="00">00</option>
                             <option class="bg-gray-700" value="15">15</option>
                             <option class="bg-gray-700" value="30">30</option>
@@ -36,15 +36,17 @@
                 </div>
                 <div>
                 <label class="text-white font-bold" for="event_start">Konec dela</label><br>
+                <div id="end-time-tooltip" class="hidden text-red-500 text-sm mt-1">End time must be greater than start time.</div>
+
                 <div class="mt-2 py-2 bg-transparent rounded-lg text-gray-100">
                     <div class="flex">
-                    <select name="event_hours_end" class="bg-transparent text-xl appearance-none border rounded-lg border-gray-400">
+                    <select id="event_hours_end" name="event_hours_end" class="bg-transparent text-xl appearance-none border rounded-lg border-gray-400">
                         @foreach (range(00, 23) as $item) {
                         <option class="bg-gray-700" value="{{ sprintf('%02d', $item) }}">{{ $item }}</option>
                         @endforeach
                     </select>
                     <span class="text-xl p-2">:</span>
-                    <select name="event_minutes_end" class="bg-transparent text-xl appearance-none border rounded-lg border-gray-400">
+                    <select id="event_minutes_end" name="event_minutes_end" class="bg-transparent text-xl appearance-none border rounded-lg border-gray-400">
                         <option class="bg-gray-700" value="00">00</option>
                         <option class="bg-gray-700" value="15">15</option>
                         <option class="bg-gray-700" value="30">30</option>
@@ -55,8 +57,35 @@
                 </div>
             </div>
             @livewire('event-dropdowns')
-            <input class="bg-green-600 hover:bg-green-500 rounded-md text-white text-sm p-4 min-w-32 my-2" type="submit" value="Potrdi vnos">
+            <input id="submit-button" class="bg-green-600 hover:bg-green-500 rounded-md text-white text-sm p-4 min-w-32 my-2" type="submit" value="Potrdi vnos">
         </form>
     </div>
   </div>
 </div>
+<script>
+    function checkEndTime() {
+        const eventHoursStart = document.getElementById('event_hours_start');
+        const eventMinutesStart = document.getElementById('event_minutes_start');
+        const eventHoursEnd = document.getElementById('event_hours_end');
+        const eventMinutesEnd = document.getElementById('event_minutes_end');
+        const tooltip = document.getElementById('end-time-tooltip');
+        const submitButton = document.getElementById('submit-button');
+
+        const startTime = parseInt(eventHoursStart.value) * 60 + parseInt(eventMinutesStart.value);
+        const endTime = parseInt(eventHoursEnd.value) * 60 + parseInt(eventMinutesEnd.value);
+
+        if (endTime <= startTime) {
+            tooltip.classList.remove('hidden');
+            submitButton.disabled = true;
+        } else {
+            tooltip.classList.add('hidden');
+            submitButton.disabled = false;
+        }
+    }
+
+    document.getElementById('event_hours_start').addEventListener('change', checkEndTime);
+    document.getElementById('event_minutes_start').addEventListener('change', checkEndTime);
+    document.getElementById('event_hours_end').addEventListener('change', checkEndTime);
+    document.getElementById('event_minutes_end').addEventListener('change', checkEndTime);
+</script>
+
