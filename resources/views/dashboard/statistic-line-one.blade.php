20345eecd5caa61152eec46e2f2c1496392c4336
Leeloop <leon@leeloop.si>
2023-03-23 08:12:02 +0000

    ure

--- resources/views/dashboard/statistic-line-one.blade.php
+++ resources/views/dashboard/statistic-line-one.blade.php
@@ -21,7 +21,7 @@
                 <dd class="mt-1 text-2xl font-bold tracking-tight text-white">{{ $workingSeconds / 3600 }}</dd>
             </div>
             <div class="relative w-auto pl-4 flex-initial">
-                <div class="text-white p-2 text-center inline-flex items-center justify-center w-12 h-12 shadow-lg rounded-full bg-pink-600">
+                <div class="text-white p-2 text-center inline-flex items-center justify-center w-12 h-12 shadow-lg rounded-full bg-red-600">
                     <i class="w-auto fa-solid fa-chart-line"></i>
                 </div>
             </div>
@@ -42,6 +42,25 @@
         </div>
         <div class="mt-4 text-white text-xs">
             <span class="font-bold bg-green-500 p-1 rounded-md">{{ number_format($totalWorkingDaysProcents, 2, '.', ',') }} %</span>&nbsp; od celote
+        </div>
+    </div>
+    <div class="overflow-hidden rounded-lg bg-white bg-opacity-10 px-4 py-5 shadow sm:p-6 border-white border-opacity-25 border shadow-md">
+        <div class="flex flex-wrap">
+            <div class="relative w-full pr-4 max-w-full flex-grow flex-1 truncate text-xs font-black uppercase text-white">
+                + oz - ure
+                <dd class="mt-1 text-2xl font-bold tracking-tight text-white">
+                    {{ ($totalWorkingDays * 8 > $workingSeconds / 3600) ? '-' : '' }}{{ abs($totalWorkingDays * 8 - ($workingSeconds / 3600)) }}
+                </dd>
+            </div>
+            <div class="relative w-auto pl-4 flex-initial">
+                <div class="text-white p-2 text-center inline-flex items-center justify-center w-12 h-12 shadow-lg rounded-full bg-pink-500">
+                    <i class="fa-solid fa-clock"></i>
+                </div>
+            </div>
+        </div>
+        <div class="mt-4 text-white text-xs">
+            <span class="font-bold bg-pink-500 p-1 rounded-md">{{ number_format(($totalWorkingDays * 8 - ($workingSeconds / 3600)) / ($totalWorkingDays * 8) * 100, 2) }}%
+ %</span>&nbsp; od celote
         </div>
     </div>
     </dl>
