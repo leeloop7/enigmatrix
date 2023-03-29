a63d87e3a747036436b3c36c011d5fc05361251d
Leeloop <leon@leeloop.si>
2023-03-29 08:17:41 +0000

    poslovni

--- resources/views/livewire/event-dropdowns.blade.php
+++ resources/views/livewire/event-dropdowns.blade.php
@@ -19,7 +19,7 @@
     @endif
     </div>
     <div>
-    @if(in_array($event_theme, [1, 9, 10]))
+    @if(in_array($event_theme, [1, 9, 10, 13]))
         @livewire('dropdowns', ['jobId' => $event_theme])
         <label class="text-white font-bold mt-4" for="event_theme">Vrsta dela</label><br>
         <select name="event_jobdesc" class="block appearance-none w-full bg-transparent border-gray-400 px-4 py-1 pl-2 pr-8 mt-1 mb-4 rounded-lg text-gray-100 text-sm">
